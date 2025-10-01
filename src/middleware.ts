8717851116505f8a8b77cef9c9f5a84f7cf87002
محمد إدريس <softwarepowerline@gmail.com>
2025-10-01 09:26:24 +0000

    لم تحل المشكلة

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
-  // A list of all locales that are supported
   locales: ['en', 'ar'],
- 
-  // Used when no locale matches
   defaultLocale: 'en'
 });
  
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(ar|en)/:path*']
+  // Skip all paths that should not be internationalized
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
